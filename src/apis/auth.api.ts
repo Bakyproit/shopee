434b3d8d4f0f709c124841c3678b6716a957fbd3
Ba Ky <baky2311@gmail.com>
2023-07-28 16:20:51 +0700

    v5

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -6,7 +6,7 @@ import http from 'src/utils/http'
 const authApi = {
   registerAccount: (body: { email: string; password: string }) => http.post<AuthResponse>('/register', body),
   login: (body: { email: string; password: string }) => http.post<AuthResponse>('/login', body),
-  logout: () => http.post('/logouts'),
+  logout: () => http.post('/logout'),
   refreshToken: (body: string) => http.post('/refresh-access-token', body)
 }
 
